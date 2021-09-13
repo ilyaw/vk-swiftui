d0b8e2749d28b8052cbdb74e302f0ca8cc71e798
ilya rudenko <webilya29@gmail.com>
2021-09-13 16:16:45 +0300

    Create project

---
+++ VKClient/VKClient/VKClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKClientApp.swift
+//  VKClient
+//
+//  Created by Ilya on 13.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct VKClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
