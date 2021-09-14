4a308c1ed5b872c350ca103de1be8e218d2d89d6
ilya rudenko <webilya29@gmail.com>
2021-09-14 22:06:16 +0300

    Add login screen

--- VKClient/VKClient/ContentView.swift
+++ VKClient/VKClient/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var login: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ScrollView {
+                VStack {
+                    Image("VK_Full_Logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding(30)
+                    HStack {
+                        Text("Login:")
+                        Spacer()
+                        TextField("", text: $login)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .frame(maxWidth: 250)
+                    }.padding(.horizontal, 20)
+                    HStack {
+                        Text("Password:")
+                        Spacer()
+                        SecureField("", text: $password)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .frame(maxWidth: 250)
+                    }.padding(.horizontal, 20)
+                    Button("Войти") {
+                        print("Hello world!")
+                    }
+                    .buttonStyle(RoundedRectangleButtonStyle())
+                    .cornerRadius(5.0)
+                    .frame(maxWidth: 300)
+                    .padding(.ArrayLiteralElement(arrayLiteral: [.top, .bottom]), 5)
+                    .disabled(login.isEmpty || password.isEmpty)
+                }
+            }
+            
+        }
     }
 }
 
---
+++ VKClient/VKClient/Extensions/Color + Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Color + Extension.swift
+//  VKClient
+//
+//  Created by Ilya on 14.09.2021.
+//
+
+import SwiftUI
+
+extension Color {
+    static var vkBlue = Color(#colorLiteral(red: 0.1529411765, green: 0.5294117647, blue: 0.9607843137, alpha: 1))
+}
